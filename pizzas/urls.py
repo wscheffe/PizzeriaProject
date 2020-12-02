f738ed06616024363e7ab1b9b97bef9f8ff24e9a
wscheffe <will.scheffe@gmail.com>
2020-12-02 13:47:54 -0600

    good

---
+++ pizzas/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+
+app_name = 'pizzas'
+
+
+urlpatterns = [
+
+path('', views.index, name='index'),
+path('pizzas', views.pizzas, name = 'pizzas'),
+
+]
--- pizzas/views.py
+++ pizzas/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Pizza
 
 # Create your views here.
+
+def index(request):
+    """The  home page for pizzas """
+    return render(request, 'pizzas/index.html')
+
+
+def pizzas(request):
+    pizzas = Pizza.objects.order_by('date_added')
+    context = {'pizzas':pizzas}
+    return render(request, 'pizzas/pizzas.html', context)
